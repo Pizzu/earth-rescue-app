e587f995f2f87fd75763aecc554dff996c7dd516
loforteluca <lucaloforte3@gmail.com>
2022-08-15 18:51:42 +0200

    build: implement storybook and configure it

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "staticDirs": ["../public"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
