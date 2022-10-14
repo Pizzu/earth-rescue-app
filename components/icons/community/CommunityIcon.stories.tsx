b50dbab12b91b6d357809df8aa26ea07c713e85e
loforteluca <lucaloforte3@gmail.com>
2022-10-14 22:30:46 +0200

    ci: import CommunityIcon component

---
+++ components/icons/community/CommunityIcon.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ISvgIcon } from '../../../types/icons';
+import CommunityIcon from './CommunityIcon';
+import { mockCommunityIconProps } from './CommunityIcon.mocks';
+
+export default {
+  title: 'icons/CommunityIcon',
+  component: CommunityIcon,
+  argTypes: [],
+} as ComponentMeta<typeof CommunityIcon>;
+
+const Template: ComponentStory<typeof CommunityIcon> = (args) => {
+  return <CommunityIcon {...args} />;
+};
+
+export const CommunityIconBase = Template.bind({});
+export const CommunityIconHighlighted = Template.bind({});
+
+CommunityIconBase.args = {
+  ...mockCommunityIconProps.communityIcon,
+} as ISvgIcon;
+
+CommunityIconHighlighted.args = {
+  ...mockCommunityIconProps.communityIconHighlighted,
+} as ISvgIcon;
--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as CommunityIcon } from './community/CommunityIcon';
 export { default as GoalsIcon } from './goals/GoalsIcon';
 export { default as HouseIcon } from './house/HouseIcon';
 export { default as NewsIcon } from './news/NewsIcon';
