50a5324c9eadf5dd8e723abababc8b3f9aede41f
loforteluca <lucaloforte3@gmail.com>
2022-10-14 18:02:15 +0200

    ci: add GoalsIcon component

---
+++ components/icons/goals/GoalsIcon.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ISvgIcon } from '../../../types/icons';
+import GoalsIcon from './GoalsIcon';
+import { mockGoalsIconProps } from './GoalsIcon.mocks';
+
+export default {
+  title: 'icons/GoalsIcon',
+  component: GoalsIcon,
+  argTypes: [],
+} as ComponentMeta<typeof GoalsIcon>;
+
+const Template: ComponentStory<typeof GoalsIcon> = (args) => {
+  return <GoalsIcon {...args} />;
+};
+
+export const GoalsIconBase = Template.bind({});
+export const GoalsIconHighlighted = Template.bind({});
+
+GoalsIconBase.args = {
+  ...mockGoalsIconProps.goalsIcon,
+} as ISvgIcon;
+
+GoalsIconHighlighted.args = {
+  ...mockGoalsIconProps.goalsIconHighlighted,
+} as ISvgIcon;
--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,2 +1,3 @@
+export { default as GoalsIcon } from './goals/GoalsIcon';
 export { default as HouseIcon } from './house/HouseIcon';
 export { default as TreesIcon } from './trees/TreesIcon';
